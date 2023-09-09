84516b7cb23d22683ab5afbfa4ccc54cceb4f110
LuisLeon06 <138534490+LuisLeon06@users.noreply.github.com>
2023-09-09 15:59:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const { request } = require('http');
 let path = require('path')
 const app = express();
 
+app.use(express.static("public"))
 app.get('/', (request , response) => {
     response.sendFile(path.join(__dirname, "../mercadoliebre/views/home.html"));
 });
-app.use(express.static(path.join(__dirname,"../mercadoliebre/public/styles.css")));
 
 app.listen(2020, () => {
     console.log("[servidor]:servidor corriendo en el puerto 2020") 
